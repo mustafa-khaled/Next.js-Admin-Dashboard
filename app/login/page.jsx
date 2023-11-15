29dd4d331ee708627f21a55695f00017fa55c67d
Mustafa <mostafakhaled0787314@gmail.com>
2023-11-15 21:47:11 +0200

    create login form and single product page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,5 +1,16 @@
-function Login() {
-  return <div>Login</div>;
+import styles from "@/app/ui/login/login.module.css";
+import LoginForm from "../ui/login/loginForm/loginForm";
+
+function LoginPage() {
+  return (
+    <div className={styles.container}>
+      {/* <form className={styles.form}>
+        <input type="text" placeholder="User Name" />
+        <input type="text" placeholder="Password" />
+      </form> */}
+      <LoginForm />
+    </div>
+  );
 }
 
-export default Login;
+export default LoginPage;
