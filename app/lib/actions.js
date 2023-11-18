b8e80df7acf7891bc8b275e7671ad47dee53e576
Mustafa <mostafakhaled0787314@gmail.com>
2023-11-18 10:18:41 +0200

    create new user action

---
+++ app/lib/actions.js
@@ -0,0 +1,35 @@
+import { revalidatePath } from "next/cache";
+import { User } from "./models";
+import { connectToDB } from "./utils";
+import { redirect } from "next/navigation";
+
+import bcrypt from "bcrypt";
+
+export const addUser = async (formData) => {
+  "use server";
+  const { username, email, password, phone, address, isAdmin, isActive } =
+    Object.fromEntries(formData);
+  try {
+    connectToDB();
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      phone,
+      address,
+      isAdmin,
+      isActive,
+    });
+    await newUser.save();
+  } catch (err) {
+    console.log(err);
+    throw new Error(err);
+  }
+
+  revalidatePath("/dashboard/users");
+  redirect("/dashboard/users");
+};
