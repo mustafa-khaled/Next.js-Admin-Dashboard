23ebefba97ad5bc0e221adfc729c9e35f19e72d5
Mustafa <mostafakhaled0787314@gmail.com>
2023-11-18 13:00:31 +0200

    create auth

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -4,10 +4,6 @@ import LoginForm from "../ui/login/loginForm/loginForm";
 function LoginPage() {
   return (
     <div className={styles.container}>
-      {/* <form className={styles.form}>
-        <input type="text" placeholder="User Name" />
-        <input type="text" placeholder="Password" />
-      </form> */}
       <LoginForm />
     </div>
   );
